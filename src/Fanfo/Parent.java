f0d96675743fafafd5f76fc24f6c6151f391f827
vivek <kumarrawanivivek@gmail.com>
2024-08-21 18:36:17 +0530

    more codes:

---
+++ src/Fanfo/Parent.java
@@ -0,0 +1,4 @@
+package Fanfo;
+
+public class Parent {
+}
---
+++ src/templates/IsNthRoot.java
@@ -0,0 +1,7 @@
+package templates;
+
+public class Index {
+    public static void main(String[] args) {
+        System.out.println(Exp.exponent(7, 2));
+    }
+}
